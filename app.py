4cf0628fc4b998b0ed6fc20a7cd3f2b4f5df6473
Shawn Yong <shawn.yong3@gmail.com>
2015-11-15 18:15:48 -0800

    db

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
-import models
+from models import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-print(models.BankAccount.__table__)
+
+engine = db_connect()
+create_tables(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+shawn_account = BankAccount(username='shawn')
+session.add(shawn_account)
+session.commit()
+
+def add_transaction(username, amount):
+    bank_account = session.query(BankAccount).filter_by(username=username).first()
+    new_transaction = Transaction(bank_account_id=bank_account.id, amount=amount)
+    print(new_transaction.bank_account_id)
+    session.add(new_transaction)
+    session.commit()
+
+add_transaction('shawn', -23)
+add_transaction('shawn', 12)
+add_transaction('shawn', -11)
+add_transaction('shawn', 24)
--- models.py
+++ models.py
@@ -2,33 +2,23 @@ from sqlalchemy import *
 from sqlalchemy.engine.url import URL
 from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.engine.url import URL
+import settings
 
 DeclarativeBase = declarative_base()
 
-DATABASE = {
-    'drivername': 'postgres',
-    'host': 'localhost',
-    'port': '5432',
-    'username': 'postgres',
-    'password': 'postgres',
-    'database': 'rpg'
-}
-
-def create_character_table(engine):
+def create_tables(engine):
     DeclarativeBase.metadata.create_all(engine)
 
 def db_connect():
-    return create_engine(URL(DATABASE))
-
-def create_character_table(engine):
-    DeclarativeBase.metadata.create_all(engine)
+    return create_engine(URL(**settings.DATABASE))
 
 class BankAccount(DeclarativeBase):
-    __tablename__ = "bank_account"
-    username = Column('username', String, primary_key=True)
+    __tablename__ = 'bank_account'
+    id = Column(Integer, primary_key=True)
+    username = Column('username', String)
 
 class Transaction(DeclarativeBase):
     __tablename__ = "transaction"
-    username = Column('username', String, ForeignKey('bank_account.username'))
+    id = Column(Integer, primary_key=True)
+    bank_account_id = Column('bank_account_id', Integer, ForeignKey('bank_account.id'))
     amount = Column('amount', Integer)
